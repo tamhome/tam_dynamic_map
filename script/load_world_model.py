b17f475be314bd6d40193ff08eba7fa398876726
yuzoo0226 <fryuzoo@gmail.com>
2023-12-10 19:41:55 +0900

    add skill for set collision world

--- script/load_world_model.py
+++ script/load_world_model.py
@@ -7,6 +7,8 @@ import rospy
 import roslib
 import pprint
 
+from hsrlib.hsrif import collision_world
+
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Pose, Point, Quaternion
 
@@ -15,11 +17,15 @@ from tamlib.node_template import Node
 
 class LoadWorldModel(Node):
     def __init__(self) -> None:
+        super().__init__()
         self.furniture_dir = roslib.packages.get_pkg_dir("tam_dynamic_map") + "/io/furniture_templates/"
         self.yaml_path = roslib.packages.get_pkg_dir("tam_dynamic_map") + "/io/map/sample.yaml"
 
-        self.marker_array_publisher = rospy.Publisher('dynamic_map', MarkerArray, queue_size=10)
-        # self.marker_array = MarkerArray()
+        self.marker_array_publisher = rospy.Publisher('/dynamic_map/semi_dynamic_markers', MarkerArray, queue_size=10)
+        self.collision_world = collision_world.CollisionWorld()
+
+        # collision_worldの初期化フラグ
+        self.initialize = True
 
     def load_marker_poses(self, model_type: str, marker_ns: str, offset_pose={"x": 0, "y": 0, "z": 0}, scale={"x": 1, "y": 1, "z": 1}) -> dict:
         """家具のモデルとなるyamlを読み込む関数
@@ -43,6 +49,10 @@ class LoadWorldModel(Node):
             pose = content["pose"]
             size = content["size"]
 
+            pub_pose_x = offset_pose["x"] + (pose["x"] * scale["x"])
+            pub_pose_y = offset_pose["y"] + (pose["y"] * scale["y"])
+            pub_pose_z = offset_pose["z"] + (pose["z"] * scale["z"])
+
             marker = Marker()
             marker.header.frame_id = "map"  # フレームIDを適切な値に変更
             marker.header.stamp = rospy.Time.now()
@@ -50,18 +60,29 @@ class LoadWorldModel(Node):
             marker.id = i
             if marker_type == "box":
                 marker.type = Marker.CUBE
+                if self.initialize:
+                    self.loginfo(f"pub collision world: {marker_ns}")
+                    self.collision_world.add_box(
+                        size["x"] * scale["x"],
+                        size["y"] * scale["y"],
+                        size["z"] * scale["z"],
+                        pose=[(pub_pose_x, pub_pose_y, pub_pose_z), (0, 0, 0, 1)],
+                        name=marker_ns
+                    )
             else:
                 marker.type = Marker.CUBE
             marker.action = Marker.ADD
-            marker.pose = Pose(Point(pose['x'] + offset_pose["x"], pose['y'] + offset_pose["y"], pose['z'] + offset_pose["z"]), Quaternion(0, 0, 0, 1))
-            marker.scale = Point(size["x"], size["y"], size["z"])
+            marker.pose = Pose(Point(pub_pose_x, pub_pose_y, pub_pose_z), Quaternion(0, 0, 0, 1))
+            marker.scale.x = size["x"] * scale["x"]
+            marker.scale.y = size["y"] * scale["y"]
+            marker.scale.z = size["z"] * scale["z"]
             marker.color.r = 1.0
             marker.color.g = 0.0
             marker.color.b = 0.0
             marker.color.a = 1.0
             marker_array.markers.append(marker)
 
-        # namespaceを合わせて配信
+        # namespaceを可視化
         marker = Marker()
         marker.header.frame_id = "map"  # フレームIDを適切な値に変更
         marker.header.stamp = rospy.Time.now()
@@ -70,8 +91,8 @@ class LoadWorldModel(Node):
         marker.id = 9999
         marker.type = Marker.TEXT_VIEW_FACING
         marker.action = Marker.ADD
-        marker.pose = Pose(Point(offset_pose["x"], offset_pose["y"], offset_pose["z"] + scale["z"] + 0.2), Quaternion(0, 0, 0, 1))
-        marker.scale = Point(0, 0, 0.3)  # 文字の大きさ指定
+        marker.pose = Pose(Point(offset_pose["x"], offset_pose["y"], offset_pose["z"] + scale["z"] + 0.1), Quaternion(0, 0, 0, 1))
+        marker.scale = Point(0, 0, 0.1)  # 文字の大きさ指定
         marker.color.r = 0.0
         marker.color.g = 0.0
         marker.color.b = 0.0
@@ -82,7 +103,7 @@ class LoadWorldModel(Node):
 
         # return marker_poses
 
-    def load_world(self, yaml_path: str) -> dict:
+    def load_world_model(self, yaml_path: str) -> dict:
         """マップモデルのyamlを読み込む関数
         Args:
             yaml_path(str): 対象とするyamlファイルのpath
@@ -93,17 +114,36 @@ class LoadWorldModel(Node):
             world_model = yaml.safe_load(file)["world"]
         return world_model
 
+    def set_collision_world(self) -> bool:
+        """world_modelに配信されている情報をcollsion_worldにも登録する
+        Args:
+
+        Return:
+            bool 登録に成功したらTrue
+        """
+        pass
+
     def run(self) -> None:
-        world_model = self.load_world(self.yaml_path)
+        if self.initialize:
+            self.loginfo("remove all collison world's objetcs")
+            self.collision_world.remove_all()
+
+        world_model = self.load_world_model(self.yaml_path)
         print(world_model)
 
         for target_model in world_model:
             print(target_model["id"])
             print(target_model["pose"])
-            marker_array = self.load_marker_poses(target_model["type"], target_model["id"], target_model["pose"])
+            if target_model != "wall":
+                marker_array = self.load_marker_poses(target_model["type"], target_model["id"], target_model["pose"], target_model["scale"])
+            else:
+                # 壁は画像から読み込みを行う
+                pass
 
             # MarkerArrayをパブリッシュ
             self.marker_array_publisher.publish(marker_array)
+
+        self.initialize = False
 
 
 if __name__ == "__main__":
